cf7301e22656d84fb7b1932522a06c88b0848e29
Joan Sebastian Tamayo Rivera <pwnaoj@gmail.com>
2025-06-05 11:26:33 -0500

    refactor: using lxml

--- utils/namespaces.py
+++ utils/namespaces.py
@@ -1,9 +1,6 @@
 """utils/namespaces.py"""
 
-import io
-import re
-import xml.etree.ElementTree as ET
-
+from lxml import etree
 from xml.etree.ElementTree import ParseError
 
 
@@ -27,6 +24,7 @@ def is_valid_xml(xml_string: str, type_service: str):
 def extract_namespaces(xml_content: str, type_service: str) -> dict:
     """
     Extrae los namespaces de los XML que están en los campos requestService y responseService.
+    Usa lxml con recover=True para manejar prefijos no declarados.
 
     Args:
         xml_content (str): XML de requestService/responseService.
@@ -36,13 +34,17 @@ def extract_namespaces(xml_content: str, type_service: str) -> dict:
     """
     try:
         if is_valid_xml(xml_content, type_service):
-            namespaces = {}
-            xml_file = io.StringIO(xml_content)
+            # Parser lxml con recuperación automática de errores
+            parser = etree.XMLParser(recover=True)
             
-            for event, elem in ET.iterparse(xml_file, events=('start-ns',)):
-                _, uri = elem
-                namespaces[uri] = None
+            # Parsear el XML tolerando prefijos no declarados
+            root = etree.fromstring(xml_content.encode('utf-8'), parser)
+            
+            # Extraer namespaces usando nsmap de lxml
+            # nsmap incluye todos los namespaces conocidos en el contexto
+            namespaces = {uri: None for uri in root.nsmap.values() if uri}
             
             return namespaces
+            
     except Exception:
         raise
