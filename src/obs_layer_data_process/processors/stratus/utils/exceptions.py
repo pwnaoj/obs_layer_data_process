1a0efad447a84cd1e52f5d25d5ce2215f45009e3
pwnaoj <pwnaoj@gmail.com>
2025-08-04 12:42:12 -0500

    refactor: Hacer opcional el argumento 'detail' de la excepción.

--- src/obs_layer_data_process/processors/stratus/utils/exceptions.py
+++ src/obs_layer_data_process/processors/stratus/utils/exceptions.py
@@ -26,7 +26,7 @@ class InvalidEventDataError(StratusProcessorError):
     """
     Se lanza cuando los datos del evento son inválidos o están incompletos.
     """
-    def __init__(self, detail: str):
+    def __init__(self, detail: str = "Datos del evento no disponibles"):
         super().__init__(
             message=f"Datos del evento inválidos o incompletos: {detail}"
         )
